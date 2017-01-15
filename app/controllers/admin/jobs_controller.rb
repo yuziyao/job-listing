aef57b217db37849902552e9300a66f82385f001
yoyo <yuziyao2007@163.com>
2017-01-15 12:02:49 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,5 @@
 class Admin::JobsController < ApplicationController
-  class Admin::JobsController < ApplicationController
+  
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
   def show
@@ -50,6 +50,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
   end
 end
