9d4043bf2a02236e6f66143898027835a3992d23
yoyo <yuziyao2007@163.com>
2017-01-15 17:22:39 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,11 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
-    end
 
-  end
 
-  resources :jobs do
+
     resources :resumes
   end
-
+end
   root 'jobs#index'
 end
