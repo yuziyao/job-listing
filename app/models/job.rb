decac6a3498aab99a70d8d5bf356183e54bc58a3
yoyo <yuziyao2007@163.com>
2017-01-15 17:03:05 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,11 @@
 class Job < ApplicationRecord
   scope :recent, -> {  order('created_at DESC') }
+  scope :published, -> { where(is_hidden: false) }
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  has_many :resumes
 
   def publish!
       self.is_hidden = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+
   namespace :admin do
     resources :jobs do
       member do
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
       end
     end
 
+  end
+
+  resources :jobs do
+    resources :resumes
   end
 
   root 'jobs#index'
