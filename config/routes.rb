2be711e202fe82529c93346057242166dc34daf1
yoyo <yuziyao2007@163.com>
2017-01-20 14:14:23 +0800

    All the things

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,20 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :jobs do
+
       member do
         post :publish
         post :hide
       end
 
-
-
     resources :resumes
   end
 end
-  root 'jobs#index'
+resources :jobs do
+  resources :resumes
+
+end
+
+
+ root 'welcome#index'
 end
